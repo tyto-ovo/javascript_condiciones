e2e5e3d5901987c29c3747460a950b0ea253e8a5
tyto-ovo <sjollyparker@gmail.com>
2024-08-03 21:31:14 -0400

    stickers listos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// -------- BORDES -------- //
+
 const imagen = document.querySelector(".foto");
 let contador = 0;
 const borde = () => {
@@ -9,3 +11,88 @@ const borde = () => {
     imagen.style.border = "none";
   }
 };
+// -------- ------ -------- //
+
+// -------- STICKERS -------- //
+let cantidadUno = document.querySelector(".cantidad-sticker-1");
+let cantidadDos = document.querySelector(".cantidad-sticker-2");
+let cantidadTres = document.querySelector(".cantidad-sticker-3");
+let total = document.querySelector(".cantidad-total");
+
+const suma = () => {
+  let contador =
+    Number(cantidadUno.innerHTML) +
+    Number(cantidadDos.innerHTML) +
+    Number(cantidadTres.innerHTML);
+  total.innerHTML = contador;
+};
+const comprobar = () => {
+  let seccion = document.querySelector(".total-section");
+  if (Number(total.innerHTML) > 10) {
+    seccion.innerHTML = "<p> Llevas demasiados stickers</p>";
+  } else {
+    seccion.innerHTML = `<p> Llevas ${total.innerHTML} stickers.</p>`;
+  }
+};
+
+const contadorMenosUno = () => {
+  let numeroUnoMenos = Number(cantidadUno.innerHTML);
+  if (numeroUnoMenos > 0) {
+    numeroUnoMenos = numeroUnoMenos - 1;
+    cantidadUno.innerHTML = numeroUnoMenos;
+
+    suma();
+    comprobar();
+  }
+};
+const contadorMasUno = () => {
+  let numeroUnoMas = Number(cantidadUno.innerHTML);
+  numeroUnoMas = numeroUnoMas + 1;
+  cantidadUno.innerHTML = numeroUnoMas;
+
+  suma();
+  comprobar();
+};
+const contadorMenosDos = () => {
+  let numeroDosMenos = Number(cantidadDos.innerHTML);
+  if (numeroDosMenos > 0) {
+    numeroDosMenos = numeroDosMenos - 1;
+    cantidadDos.innerHTML = numeroDosMenos;
+
+    suma();
+    comprobar();
+  }
+};
+const contadorMasDos = () => {
+  let numeroDosMas = Number(cantidadDos.innerHTML);
+  numeroDosMas = numeroDosMas + 1;
+  cantidadDos.innerHTML = numeroDosMas;
+
+  suma();
+  comprobar();
+};
+
+const contadorMenosTres = () => {
+  let numeroTresMenos = Number(cantidadTres.innerHTML);
+  if (numeroTresMenos > 0) {
+    numeroTresMenos = numeroTresMenos - 1;
+    cantidadTres.innerHTML = numeroTresMenos;
+
+    suma();
+    comprobar();
+  }
+};
+const contadorMasTres = () => {
+  let numeroTresMas = Number(cantidadTres.innerHTML);
+  numeroTresMas = numeroTresMas + 1;
+  cantidadTres.innerHTML = numeroTresMas;
+
+  suma();
+  comprobar();
+};
+
+// -------- -------- -------- //
+
+// -------- CONTRASEÑAS -------- //
+
+// -------- ----------- -------- //
